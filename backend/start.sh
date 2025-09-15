e4d1c8cd7284a49759672427ceb3a0af207399ba
roxy1102 <hanerokheya11@gmail.com>
2025-09-15 12:04:45 +0000

    backend problem

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-php artisan migrate --force
+# Suppression de la migration automatique pour éviter l'erreur de connexion à la base de données
+# php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
