bc4f67aa6e30500da411203583a6a3d14a327558
roxy1102 <hanerokheya11@gmail.com>
2025-09-16 00:42:36 +0000

    update

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Illuminate\Http\Middleware\HandleCors::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
